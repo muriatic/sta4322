720f3ca1f45797858bf1d3ac384259a5f2da04f0
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2025-07-25 15:00:54 -0400

    includes power graph

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ class PowerBeta(Scene):
         sigma = 1.5
         alpha = 0.05
         c = norm.ppf(1 - alpha, loc=mu_0, scale=sigma)
+        mu_a_target = c - sigma * norm.ppf(0.05)
 
+        # x-range for both main and power subplot
+        x_min = -mu_a_target - 5
+        x_max = mu_a_target + 5
+
+        # Main distribution axes (same as before)
         axes = Axes(
-            x_range=[-4, 7, 1],
+            x_range=[x_min, x_max, 1],
             y_range=[0, 0.3, 0.1],
             x_length=9,
             y_length=3,
-            axis_config={"color": WHITE, "include_tip": False}
+            axis_config={"color": WHITE, "include_tip": False, "include_ticks": False},
         )
 
         mu_a_tracker = ValueTracker(mu_0)
@@ -24,12 +30,10 @@ class PowerBeta(Scene):
         alt_curve = always_redraw(
             lambda: axes.plot(lambda x: norm.pdf(x, mu_a_tracker.get_value(), sigma), color=RED)
         )
-
         null_line = axes.get_vertical_line(axes.c2p(mu_0, 0), color=WHITE)
         alt_line = always_redraw(
             lambda: axes.get_vertical_line(axes.c2p(mu_a_tracker.get_value(), 0), color=RED)
         )
-
         type_ii = always_redraw(
             lambda: axes.get_area(
                 axes.plot(lambda x: norm.pdf(x, mu_a_tracker.get_value(), sigma),
@@ -50,12 +54,6 @@ class PowerBeta(Scene):
         alt_hyp.next_to(null_hyp, DOWN, aligned_edge=LEFT)
         hypotheses = VGroup(null_hyp, alt_hyp).to_corner(UL).scale(0.95)
 
-        mu0_label = MathTex(r"\mu_0", color=WHITE).next_to(axes.c2p(mu_0, 0), DOWN, buff=0.3)
-        mua_label = always_redraw(lambda: MathTex(r"\mu_a", color=RED).next_to(
-            axes.c2p(mu_a_tracker.get_value(), 0),
-            DOWN,
-            buff=0.85  # Larger buff than mu0_label, pushes mu_a further below x-axis
-        ))
 
         def power_val():
             val = 1 - norm.cdf(c, loc=mu_a_tracker.get_value(), scale=sigma)
@@ -65,28 +63,90 @@ class PowerBeta(Scene):
             val = norm.cdf(c, loc=mu_a_tracker.get_value(), scale=sigma)
             return val
 
-        power_tex = always_redraw(lambda: Tex(
-            r"Power $= {:.2f}$".format(power_val()),
-            color=RED
-        ).to_corner(UR, buff=0.8).scale(1.0))
+        metrics = always_redraw(lambda:
+            VGroup(
+                MathTex(r"\alpha", "=", "0.05", color=WHITE),
+                MathTex(r"\beta", "=", "{:.2f}".format(beta_val()), color=BLUE),
+                MathTex(r"\text{Power}", "=", "{:.2f}".format(power_val()), color=RED),
+            ).arrange(DOWN, aligned_edge=LEFT, buff=0.18)
+            .scale(0.95)
+            .next_to(hypotheses, DOWN, aligned_edge=LEFT, buff=0.5).shift(DOWN * 1.75)
+        )
 
-        beta_tex = always_redraw(lambda: Tex(
-            r"$\beta$ $= {:.2f}$".format(beta_val()),
-            color=BLUE
-        ).next_to(power_tex, DOWN, aligned_edge=LEFT, buff=0.18))
 
-        metrics = VGroup(power_tex, beta_tex)
+        # -------- Power curve subplot below --------
+        # Place it just below the main axes, matching the x-alignment of mu (for vertical alignment)
+        subplot_height = 2.0
+        subplot_axes = Axes(
+            x_range=[x_min, x_max, 1],
+            y_range=[0, 1.05, 1],
+            x_length=axes.x_length,
+            y_length=subplot_height,
+            axis_config={"color": WHITE, "include_tip": False},
+            y_axis_config={
+                "color": WHITE,
+                "numbers_to_include": [1]
+            },
+            x_axis_config={
+                "color": WHITE,
+                "include_ticks": False,
+                "include_numbers": False
+            }
+        )
 
+
+        subplot_axes.next_to(axes, DOWN, buff=1.4)
+
+        # Tracking point at mu_a along x-axis
+        mua_point = always_redraw(lambda: 
+            Line(
+                axes.c2p(mu_a_tracker.get_value(), -0.01),
+                axes.c2p(mu_a_tracker.get_value(), 0.01),
+                color=RED,
+                stroke_width=4
+            )
+        )
+
+        mua_label = always_redraw(lambda: MathTex(r"\mu_a", color=RED).next_to(
+            axes.c2p(mu_a_tracker.get_value(), 0),
+            DOWN,
+            buff=0.2
+        ))
+        axes.add(null_curve, alt_curve, null_line, alt_line, type_ii, power_area, mua_point, mua_label)
+        
+        mu0_label = MathTex(r"\mu_0", color=WHITE).next_to(subplot_axes.c2p(mu_0, 0), DOWN, buff=0.3)
+
+
+        # x label "\mu"
+        mu_xlabel = MathTex(r"\mu", color=WHITE).next_to(subplot_axes.x_axis, RIGHT, buff=0.25).shift(DOWN*0.05)
+
+        # Theoretical power function
+        def power_function(mu_a):
+            return 1 - norm.cdf(c, loc=mu_a, scale=sigma)
+        power_curve = subplot_axes.plot(power_function, x_range=[x_min, x_max], color=WHITE)
+
+        # Red dot tracking power at current mu_a
+        power_dot = always_redraw(lambda: Dot(
+            subplot_axes.c2p(mu_a_tracker.get_value(), power_function(mu_a_tracker.get_value())),
+            color=RED,
+            radius=0.10,
+        ))
+        subplot_axes.add(power_curve, power_dot, mu_xlabel)
+        subplot_axes.add(mu0_label)
+
+        plots = VGroup(axes, subplot_axes)
+        plots.move_to(ORIGIN).shift(DOWN * 0.1).shift(RIGHT * 0.5)  # Center vertically
+
+
+        # Add everything, order to ensure labels/dots are on top        
         self.add(
-            axes, null_curve, alt_curve,
-            null_line, alt_line,
-            type_ii, power_area,
-            mu0_label, mua_label,
+            plots,
             hypotheses,
             metrics
         )
 
-        self.play(mu_a_tracker.animate.set_value(3), run_time=5, rate_func=smooth)
+        # Animate as before
+        self.play(mu_a_tracker.animate.set_value(mu_a_target), run_time=10, rate_func=smooth)
         self.wait()
-        self.play(mu_a_tracker.animate.set_value(mu_0), run_time=3, rate_func=smooth)
+        self.play(mu_a_tracker.animate.set_value(mu_0), run_time=10, rate_func=smooth)
         self.wait()
